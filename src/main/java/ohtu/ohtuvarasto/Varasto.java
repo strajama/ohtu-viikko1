5d69e1e0bcd390d39adec19d26fede60bcd09122
strajama <32276786+strajama@users.noreply.github.com>
2018-11-09 13:48:31 +0200

    Update Varasto.java

--- src/main/java/ohtu/ohtuvarasto/Varasto.java
+++ src/main/java/ohtu/ohtuvarasto/Varasto.java
@@ -39,6 +39,11 @@ public class Varasto {
     }
 
     public double getTilavuus() {
+        for( int i=0; i<1; i++ ) {
+            for( int j=0; i<j; j++ ) {
+                System.out.println("virhe");
+            } 
+        }
         return tilavuus;
     }
 
