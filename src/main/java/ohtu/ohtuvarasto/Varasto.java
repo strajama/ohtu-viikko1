24f11bc2f30debd8ec699a6cbd1303ddd219ea62
strajama <32276786+strajama@users.noreply.github.com>
2018-11-09 13:36:55 +0200

    Update Varasto.java

--- src/main/java/ohtu/ohtuvarasto/Varasto.java
+++ src/main/java/ohtu/ohtuvarasto/Varasto.java
@@ -19,13 +19,13 @@ public class Varasto {
     public Varasto(double tilavuus, double alkuSaldo) { // kuormitetaan
         if (tilavuus > 0.0) {
             this.tilavuus = tilavuus;
-        } else // virheellinen, nollataan {
+        } else {
             this.tilavuus = 0.0;  // => käyttökelvoton varasto
         }
    /*     if (alkuSaldo < 0.0) {
             this.saldo = 0.0;
         } else */
-        if (alkuSaldo <= tilavuus) // mahtuu {
+        if (alkuSaldo <= tilavuus) {
             this.saldo = alkuSaldo;
         } else {
             this.saldo = tilavuus;  // täyteen ja ylimäärä hukkaan!
@@ -48,10 +48,10 @@ public class Varasto {
 
     // --- asettavat aksessorit eli setterit: ---
     public void lisaaVarastoon(double maara) {
-        if (maara < 0) // virhetilanteessa voidaan tehdä {
+        if (maara < 0) {
             return;       // tällainen pikapoistuminenkin!
         }
-        if (maara <= paljonkoMahtuu()) // omia aksessoreita voi kutsua {
+        if (maara <= paljonkoMahtuu()) {
             saldo = saldo + maara;          // ihan suoraan sellaisinaan
         } else {
             saldo = tilavuus;  // täyteen ja ylimäärä hukkaan!
