aa427966e222ae791bc9c09b89245f35a46f520c
strajama <32276786+strajama@users.noreply.github.com>
2018-11-09 13:20:06 +0200

    Update Varasto.java

--- src/main/java/ohtu/ohtuvarasto/Varasto.java
+++ src/main/java/ohtu/ohtuvarasto/Varasto.java
@@ -16,7 +16,7 @@ public class Varasto {
         }
         saldo = 0;     // oletus: varasto on tyhjä
     }
-/*
+
     public Varasto(double tilavuus, double alkuSaldo) { // kuormitetaan
         if (tilavuus > 0.0) {
             this.tilavuus = tilavuus;
@@ -24,16 +24,16 @@ public class Varasto {
         {
             this.tilavuus = 0.0;  // => käyttökelvoton varasto
         }
-        if (alkuSaldo < 0.0) {
+   /*     if (alkuSaldo < 0.0) {
             this.saldo = 0.0;
-        } else if (alkuSaldo <= tilavuus) // mahtuu
+        } else */if (alkuSaldo <= tilavuus) // mahtuu
         {
             this.saldo = alkuSaldo;
         } else {
             this.saldo = tilavuus;  // täyteen ja ylimäärä hukkaan!
         }
     }
-    */
+    
 
     // --- ottavat aksessorit eli getterit: ---
     public double getSaldo() {
